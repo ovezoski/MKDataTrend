0747ce9f243407d661d454151a048fd39f3631ef
Blagoja Ovezoski <blagojaovezoski@gmail.com>
2025-06-05 14:03:15 +0200

    Shadcn install, sidebar and some style improvements

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,93 @@
 import "./App.css";
 // import MacedoniaMap from "./pages/MacedoniaMap";
 import PelagoniaMap from "./pages/PelagoniaMap";
+import { Button } from "@/components/ui/button";
+import { Calendar, Home, Inbox, Search, Settings } from "lucide-react";
 
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarInset,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarProvider,
+} from "@/components/ui/sidebar";
 export function App() {
-  return (
-    <div className="margin-auto bg-white">
-      <div className="text-bold pb-2">Mak Stats</div>
+  const items = [
+    {
+      title: "Home",
+      url: "#",
+      icon: Home,
+    },
+    {
+      title: "Inbox",
+      url: "#",
+      icon: Inbox,
+    },
+    {
+      title: "Calendar",
+      url: "#",
+      icon: Calendar,
+    },
+    {
+      title: "Search",
+      url: "#",
+      icon: Search,
+    },
+    {
+      title: "Settings",
+      url: "#",
+      icon: Settings,
+    },
+  ];
 
-      <div className="margin-auto jc-center flex text-center">
-        {/* <MacedoniaMap /> */}
-        <PelagoniaMap />
+  return (
+    <SidebarProvider>
+      <div>
+        <Sidebar variant="floating">
+          <SidebarHeader />
+          <SidebarContent>
+            <SidebarGroup>
+              <SidebarGroupLabel>Application</SidebarGroupLabel>
+              <SidebarGroupContent>
+                <SidebarMenu>
+                  {items.map((item) => (
+                    <SidebarMenuItem key={item.title}>
+                      <SidebarMenuButton asChild>
+                        <a href={item.url}>
+                          <item.icon />
+                          <span>{item.title}</span>
+                        </a>
+                      </SidebarMenuButton>
+                    </SidebarMenuItem>
+                  ))}
+                </SidebarMenu>
+              </SidebarGroupContent>
+            </SidebarGroup>
+
+            <SidebarGroup />
+          </SidebarContent>
+          <SidebarFooter />
+        </Sidebar>
       </div>
-    </div>
+
+      <div className="margin-auto flex w-full justify-center">
+        <div className="p-1">
+          {/* <MacedoniaMap /> */}
+          <PelagoniaMap />
+
+          <div className="flex min-h-10 flex-col items-center justify-center">
+            <Button>Click me</Button>
+          </div>
+        </div>
+      </div>
+    </SidebarProvider>
   );
 }
 
